2863a89b53cb3438929ce58d42b97574e9b94a0c
Andrew Woo <andrew.woo@gmail.com>
2013-06-09 19:50:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioIs::Application.config.secret_token = 'a57ed36f7b5c553b4807521dbc750687cf520a8d9753bbcf2a3050d7e7f7ed122e475d4e4d0effee5325f8e7215e7216fbb0859b3e74028bdca8bb7b8ab086a0'
